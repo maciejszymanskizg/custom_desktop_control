53b11e1fe542b15aac79514d0a895c0e1d6c0bed
Maciej <maciej.szymanski@opensynergy.com>
2024-02-09 20:20:59 +0100

    Add DataMessage class.
    DataMessage class provides an interface to pack and unpack set of data
    items (configuration items) in a buffer to send or receive from
    internal / external controller.

---
+++ DataMessage.h
@@ -0,0 +1,47 @@
+#ifndef DATA_MESSAGE_H
+#define DATA_MESSAGE_H
+
+#include <stdint.h>
+#include <vector>
+
+struct MessageItem {
+	uint32_t id;
+	uint32_t value;
+};
+
+class DataMessage {
+	private:
+		struct MessageHeader {
+			uint32_t magic;
+			uint16_t message_type;
+			uint16_t number_of_items;
+		};
+		struct MessageHeader header;
+		std::vector<struct MessageItem> items;
+		uint8_t *data;
+		size_t data_size;
+
+	public:
+		enum MessageType {
+			MESSAGE_TYPE_INVALID_MESSAGE = 0x0,
+			MESSAGE_TYPE_REQUEST_DATA = 0x1,
+			MESSAGE_TYPE_RESPONSE_DATA = 0x2,
+			MESSAGE_TYPE_SEND_DATA = 0x4,
+			MESSAGE_TYPE_CONFIRM_DATA = 0x8
+		};
+
+		DataMessage(enum MessageType type);
+		DataMessage(const uint8_t *buffer, size_t size);
+		~DataMessage();
+
+		void addMessageItem(struct MessageItem item);
+		uint8_t *getRawData(size_t & size);
+
+		enum MessageType getMessageType(void);
+		std::vector<struct MessageItem> & getMessageItems(void);
+
+	private:
+		enum MessageType message_type;
+};
+
+#endif /* DATA_MESSAGE_H */
